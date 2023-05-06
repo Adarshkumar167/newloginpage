5d76fe0dfcbc43c11d1bec8c4127c31a5da7ba49
Adarsh <adarshkumar167@gmail.com>
2023-05-06 17:26:54 +0530

    Over

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -13,120 +13,171 @@ class _LoginState extends State<Login> {
     var ht = MediaQuery.of(context).size.height;
     var wt = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Column(children: [
-        SafeArea(
-          child: SizedBox(
-            height: ht * 0.25,
-            width: wt * 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      width: wt * 0.13,
-                      child: const DrawerButton(),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: wt * 0.12,
-                ),
-                SizedBox(
-                  height: ht * 0.25,
-                  width: wt * 0.60,
-                  child: Image.asset('assets/images/robot.png'),
-                ),
-                Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(wt * 0.015),
-                      child: Container(
-                        height: wt * 0.12,
-                        width: wt * 0.12,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            color: const Color.fromRGBO(96, 188, 250, 1),
-                            width: 4,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SafeArea(
+            child: SizedBox(
+              height: ht * 0.25,
+              width: wt * 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: wt * 0.13,
+                        child: const DrawerButton(),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: wt * 0.12,
+                  ),
+                  SizedBox(
+                    height: ht * 0.25,
+                    width: wt * 0.60,
+                    child: Image.asset('assets/images/robot.png'),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(wt * 0.015),
+                        child: Container(
+                          height: wt * 0.12,
+                          width: wt * 0.12,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              color: const Color.fromRGBO(96, 188, 250, 1),
+                              width: 4,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
                           ),
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        child: const CircleAvatar(
-                          backgroundImage: AssetImage(
-                            'assets/images/women.png',
+                          child: const CircleAvatar(
+                            backgroundImage: AssetImage(
+                              'assets/images/women.png',
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: ht * 0.05,
+          ),
+          SizedBox(
+            width: wt * 0.95,
+            child: Column(
+              children: [
+                SizedBox(
+                  width: wt * 0.85,
+                  child: Text(
+                    'Welcome',
+                    style: TextStyle(
+                        fontSize: ht * 0.03, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: ht * 0.03,
+                ),
+                SizedBox(
+                  width: wt * 0.85,
+                  child: Text(
+                    'Enter Email Id And Password',
+                    style: TextStyle(
+                        fontSize: ht * 0.02, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: ht * 0.05,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: wt * 0.05),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: ht * 0.02),
+                    decoration: const InputDecoration(
+                      hintText: 'Username',
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green,
+                          width: 4,
+                        ),
+                      ),
                     ),
+                  ),
+                ),
+                SizedBox(
+                  height: ht * 0.02,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: wt * 0.05),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: ht * 0.02),
+                    decoration: const InputDecoration(
+                      hintText: 'Password',
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.green,
+                          width: 4,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: ht * 0.05,
+                ),
+                SizedBox(
+                  width: wt * 0.85,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Code to handle button press
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Colors.teal[400], // set background color to Tosca
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10), // set rounded corners
+                      ),
+                    ),
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      'Register',
+                      style: TextStyle(color: Colors.blue, fontSize: 16),
+                    )
                   ],
-                )
+                ),
               ],
             ),
           ),
-        ),
-        SizedBox(
-          height: ht * 0.05,
-        ),
-        SizedBox(
-          width: wt * 0.95,
-          child: Column(
-            children: [
-              Text(
-                'Welcome',
-                style:
-                    TextStyle(fontSize: ht * 0.03, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: ht * 0.03,
-              ),
-              Text(
-                'Enter Email Id And Password',
-                style:
-                    TextStyle(fontSize: ht * 0.02, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: ht * 0.05,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: wt * 0.05),
-                child: TextFormField(
-                  style: TextStyle(fontSize: ht * 0.02),
-                  decoration: const InputDecoration(
-                    hintText: 'Username',
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 4,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: ht * 0.02,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: wt * 0.05),
-                child: TextFormField(
-                  style: TextStyle(fontSize: ht * 0.02),
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 4,
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ]),
+        ]),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         showSelectedLabels: false, // hide labels for selected items
